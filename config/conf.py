31dffd221cc2c8a9addd4a8dcfb75104cc92a8ca
UzJu <50813806+UzJu@users.noreply.github.com>
2022-03-06 16:56:26 +0800

    Update conf.py
    修复fake_useragent报错的问题

--- config/conf.py
+++ config/conf.py
@@ -6,12 +6,18 @@
 # @Time    : 2022/2/28 5:18 PM
 # @File    : conf.py
 
-from fake_useragent import UserAgent
-UA = UserAgent(use_cache_server=False)
+# from fake_useragent import UserAgent
+# UA = UserAgent(use_cache_server=False)
 
-headers = {
-    "UserAgent": UA.random
-}
+# headers = {
+#     "UserAgent": UA.random
+# }
+"""
+2022年3月6日 16:55
+部分用户反馈该库存在报错的问题，故此目前删除该库
+最开始使用该库是因为想用HTTP的方式实现功能，所以使用了fake_useragent
+现在实现的方式是直接调SDK所以不需要这个Fake_useragent了
+"""
 
 AK = ""
 SECRET = ""
